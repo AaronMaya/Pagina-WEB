6313681b3bbd472ac558203eb30a35f24aea0d14
AaronMaya <aaron190401@gmail.com>
2026-08-14 00:07:12 -0600

    Version Final, carrito y galeria tablas hecha

--- js/script.js
+++ js/script.js
@@ -34,3 +34,16 @@ whatsappButtons.forEach(button => {
     });
 
 });
+
+/* =========================================
+   MENÚ MÓVIL
+========================================= */
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+
+    navMenu.classList.toggle('active');
+
+});
